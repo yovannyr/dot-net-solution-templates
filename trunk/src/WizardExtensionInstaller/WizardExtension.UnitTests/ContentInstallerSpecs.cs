425005d809edd5a32901d61c0ce51c48f8a0e2b8
eric.hexter <eric.hexter@7169eda6-aaf7-11dd-91bf-632da8b6e10b>
2008-11-11 19:38:13 +0000

    refactored the solution

---
+++ trunk/src/WizardExtensionInstaller/WizardExtension.UnitTests/ContentInstallerSpecs.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.ContentInstaller;
+using NBehave.Spec.NUnit;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace WizardExtension.UnitTests
+{
+    [TestFixture]
+    public class ContentInstallerSpecs
+    {
+        [Test]
+        public void get_import_pages_should_install_null()
+        {
+            var installer = new ContentInstaller();
+            IImportPageData[] result = installer.GetImportPages();
+            result.ShouldBeNull();
+        }
+
+        [Test]
+        public void Should_import_Successfully()
+        {
+            var site = MockRepository.GenerateMock<IContentInstallerSite>();
+            IApplicationHostData[] hostdata;
+            IApplicationHostData[] appdata = new HostDataStub[1] {new HostDataStub()};
+            site.Stub(s => s.GetApplicationData("", "2.0", out hostdata)).IgnoreArguments().OutRef(new object[]
+                                                                                                       {appdata}).Return
+                (true);
+            var installer = new ContentInstaller(site);
+            string result = installer.Import(new ContentItemStub {ShouldInstall = true, ContentVersion = "2.0"});
+            result.ShouldEqual("Installation completed successfully!");
+            string asdf;
+            site.AssertWasCalled(s => s.CopyFile("", "", DuplicateFileCase.EnableOverwrite, out asdf),
+                                 s => s.IgnoreArguments());
+        }
+
+        [Test]
+        public void Should_no_opp_on_update_status()
+        {
+            var installer = new ContentInstaller();
+            installer.UpdateContentItemInstallStatus(null);
+        }
+
+        [Test]
+        public void Should_return_false_for_supports_ui()
+        {
+            var installer = new ContentInstaller();
+            bool result = installer.SupportsImportUI;
+
+            result.ShouldBeFalse();
+        }
+
+        [Test]
+        public void Should_return_false_for_unknown_content_type()
+        {
+            var installer = new ContentInstaller();
+            IContentItem[] content = new[] {new ContentItemStub {FileContentType = "unknown"}};
+            var site = MockRepository.GenerateStub<IContentInstallerSite>();
+
+            bool result = installer.AddContentItem(content, site);
+
+            result.ShouldBeFalse();
+        }
+
+        [Test]
+        public void Should_return_true_for_wizard_extension_content_type()
+        {
+            var installer = new ContentInstaller();
+            IContentItem[] content = new[] {new ContentItemStub {FileContentType = "WizardExtension"}};
+            var site = MockRepository.GenerateStub<IContentInstallerSite>();
+
+            bool result = installer.AddContentItem(content, site);
+
+            result.ShouldBeTrue();
+        }
+    }
+}
---
+++ trunk/src/WizardExtensionInstaller/WizardExtensionInstaller/ContentInstaller.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.ContentInstaller;
+
+namespace WizardExtension
+{
+    [ContentInstallerContentTypeRestriction("WizardExtension")]
+    [ContentInstallerSupportedFileSecurity(".dll")]
+    public class ContentInstaller : IImportCommunityContent
+    {
+        private IContentInstallerSite _site;
+        public ContentInstaller()
+        {
+            
+        }
+        public ContentInstaller(IContentInstallerSite site)
+        {
+            _site = site;
+        }
+
+        public bool AddContentItem(IContentItem[] contentItems, IContentInstallerSite site)
+        {
+            _site = site;
+            foreach (IContentItem contentItem in contentItems)
+            {
+                if (contentItem.FileContentType == "WizardExtension")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string Import(IContentItem contentItem)
+        {
+            if (contentItem.ShouldInstall)
+            {
+                
+                IApplicationHostData[] applicationHost;
+                //Get the addin data since it is installed with VS.  This content type does not 
+                //set any of the custom data need to located the VS installation path.
+                _site.GetApplicationData("Addin", contentItem.ContentVersion, out applicationHost);
+
+                string vsApplicationDirectory = Path.GetDirectoryName(applicationHost[0].ApplicationPath.Replace("\"",""));
+                string destinationDirectory = Path.Combine(vsApplicationDirectory, "PrivateAssemblies");
+
+                string actualDestinationPath;
+
+                string[] sourceFileNames = contentItem.GetFileNames();
+                string[] rootFileNames = contentItem.GetRootFileNames();
+
+                for (int i = 0; i < sourceFileNames.Length; i++)
+                {
+                    string destinationPath = Path.Combine(destinationDirectory, rootFileNames[i]);
+
+                    _site.StatusMessage("installing " + contentItem.DisplayName);
+
+                    _site.CopyFile(sourceFileNames[i], destinationPath,
+                                   DuplicateFileCase.EnableOverwrite | DuplicateFileCase.EnableSkip,
+                                   out actualDestinationPath);
+                }
+                
+                return "Installation completed successfully!";
+            }
+            return "Skipped";
+        }
+
+
+        public IImportPageData[] GetImportPages()
+        {
+            return null;
+        }
+
+        public void UpdateContentItemInstallStatus(IContentItem[] contentItem)
+        {
+        }
+
+        public bool SupportsImportUI
+        {
+            get { return false; }
+        }
+    }
+}
